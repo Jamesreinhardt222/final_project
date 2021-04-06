32d94315f8adcae03de6fb970b5935a9db73fd58
James Paul Reinhardt <54925215+Jamesreinhardt222@users.noreply.github.com>
2021-04-05 21:13:28 -0700

    Update server.js
    Updated server.js

--- server.js
+++ server.js
@@ -6,19 +6,26 @@ const mysql = require("mysql");
 const path = require("path");
 const app = express();
 const fetch = require("node-fetch");
+const { json } = require("express");
 const URL = "https://comp4537project.herokuapp.com";
 const POSTS = '/api/v1/posts/';
 const USERS = '/api/v1/users/';
 const LOGIN = '/api/v1/login/';
 const LOGOUT = '/api/v1/logout/';
 const STATS = '/api/v1/stats/';
+const GET = "GET";
+const POST = "POST";
+const PUT = "PUT";
+const DELETE = "DELETE";
 
 const api_endpoint = "/API/V1"
+window.localStorage.setItem('token', '');
+window.localStorage.setItem('logged_in', 'false');
+window.localStorage.setItem('user', "{}");
+
 let user = {};
-let token = ""
+// let token = ""
 
-
-console.log(process.env.SECRET);
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
@@ -39,16 +46,18 @@ const connection = mysql.createConnection({
 });
 
 
-const homeStartingContent = "Welcome to the Internet Software Architecture Blog Site!!."
-const aboutContent = "This is a site designed to demonstrate superior web architecture and CRUD technology."
-const passwordContent = "Change your password."
+const homeStartingContent = "Welcome to the Internet Software Architecture Blog Site!!";
+const aboutContent = "This is a site designed to demonstrate superior web architecture and CRUD technology.";
+const passwordContent = "Change your password.";
+const adminContent = "Here are the stats for the api requests to our server.";
 
-let logged_in = false;
+// let logged_in = false;
 
 
 app.get("/", async (req, res) => {
-
-    if (logged_in) {
+    let user = JSON.parse(window.localStorage.getItem("user"))
+    let logged_in = window.localStorage.getItem("logged_in")
+    if (logged_in == "false") {
         const response = await fetch(URL + POSTS, {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' }
@@ -75,11 +84,23 @@ app.get("/compose", (req, res) => {
     res.render("compose");
 });
 
+app.get("/admin", async (req, res) => {
+    let token = window.localStorage.getItem("token");
+    const user = { token: token }
+    const response = await fetch(URL + STATS, {
+        method: POST,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    });
+    let statistics = await response.json();
+    res.render("admin", { adminContent: adminContent, stats: statistics })
+})
+
 
 app.post(api_endpoint + "/compose", async (req, res) => {
     const post = { title: `${req.body.postTitle}`, post: `${req.body.postBody}`, token: token }
     const response = await fetch(URL + POSTS, {
-        method: 'POST',
+        method: POST,
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(post)
     });
@@ -95,18 +116,19 @@ app.get("/about", (req, res) => {
 
 
 app.get("/editPassword", (req, res) => {
-    res.render("changePassword", { passwordContent: passwordContent })
+    res.render("changeAccount", { passwordContent: passwordContent })
 })
 
 app.post("/API/V1/updatePassword", async (req, res) => {
+    let token = window.localStorage.getItem("token");
     const user = { newPassword: req.body.newPassword, token: token }
     const response = await fetch(URL + USERS, {
-        method: 'PUT',
+        method: PUT,
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
     });
     let json = await response.json()
-    console.log("updateUserPassword:", await response.json());
+    console.log("updateUserPassword:", json);
     if (json.result != "FAILED") {
         res.redirect("/")
     } else {
@@ -114,10 +136,28 @@ app.post("/API/V1/updatePassword", async (req, res) => {
     }
 })
 
+app.post("/API/V1/deleteUser", async (req, res) => {
+    let token = window.localStorage.getItem("token");
+    const user = { token: token }
+    const response = await fetch(URL + USERS, {
+        method: DELETE,
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user)
+    });
+    let data = await response.json();
+    if (data.result != "FAILED") {
+        window.localStorage.setItem("logged_in", "false");
+        res.redirect("/")
+    } else {
+        res.send("FAILED TO UPDATE PASSWORD");
+    }
+
+})
+
 app.get("/posts/:postId", async (req, res) => {
     const requestedPostId = req.params.postId;
     const response = await fetch(URL + POSTS, {
-        method: 'GET',
+        method: GET,
         headers: { 'Content-Type': 'application/json' }
     });
     let posts = []
@@ -139,7 +179,7 @@ app.get("/posts/:postId", async (req, res) => {
 app.get("/update/:postId", async (req, res) => {
     const requestedPostId = req.params.postId;
     const response = await fetch(URL + POSTS, {
-        method: 'GET',
+        method: GET,
         headers: { 'Content-Type': 'application/json' }
     });
     let posts = []
@@ -159,24 +199,26 @@ app.get("/update/:postId", async (req, res) => {
 
 app.delete(api_endpoint + "/delete/:postId", async (req, res) => {
     const requestedPostId = req.params.postId;
+    let token = window.localStorage.getItem("token");
 
     const post = { postID: requestedPostId, token: token }
     const response = await fetch(URL + POSTS, {
-        method: 'DELETE',
+        method: DELETE,
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(post)
     });
     console.log("deletePost:", await response.json());
-    req.method = "GET";
+    req.method = GET;
     res.redirect(303, "/");
 });
 
 app.post(api_endpoint + "/update/:postId", async (req, res) => {
     const requestedPostId = req.params.postId;
+    let token = window.localStorage.getItem("token");
 
     const post = { postID: requestedPostId, title: `${req.body.postTitle}`, post: `${req.body.postBody}`, token: token }
     const response = await fetch(URL + POSTS, {
-        method: 'PUT',
+        method: PUT,
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(post)
     });
@@ -185,7 +227,7 @@ app.post(api_endpoint + "/update/:postId", async (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-    logged_in = false;
+    let token = window.localStorage.setItem("logged_in", "false");
     res.redirect("/");
 })
 
@@ -195,10 +237,10 @@ app.get("/register", (req, res) => {
 });
 
 
-app.post("/register", async (req, res) => {
+app.post("/API/V1/register", async (req, res) => {
     const user = { userID: req.body.username, password: req.body.password }
     const response = await fetch(URL + USERS, {
-        method: 'POST',
+        method: POST,
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
     });
@@ -209,22 +251,24 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
 
-    user = { userID: req.body.username, password: req.body.password }
-    const response = await fetch(URL + LOGIN, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(user)
-    })
+    let user = { userID: req.body.username, password: req.body.password }
+    let userData = JSON.stringify(user)
+    window.localStorage.setItem("user", userData);
 
+    const response = await fetch(URL + LOGIN, {
+        method: POST,
+        headers: { 'Content-Type': 'application/json' },
+        body: userData
+    })
 
     let json = await response.json()
     console.log("login:", json);
 
     if (json.result != "FAILED") {
         console.log("login:", json);
-        logged_in = true;
+        window.localStorage.setItem("logged_in", "true");
         res.redirect("/");
-        token = json.token;
+        window.localStorage.setItem("token", json.token);
     } else {
         res.send("Login Failed");
     }
